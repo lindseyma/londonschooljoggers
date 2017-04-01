a0e56e51c2f65e16e2331ef6d2e93356190dd2af
StivenPeter <stivenpeter@gmail.com>
2017-04-01 12:06:02 -0400

    Html

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 #from utils import map
+import drugs
+import labor
+
+list_of_report = drugs.get_reports()
+print list_of_report 
 
 app = Flask(__name__)
 app.secret_key="idkjustsomethingrandom"
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.debug = True
