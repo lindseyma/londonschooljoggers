eb657856fe443772837be0d595fefc3f78e85682
StivenPeter <stivenpeter@gmail.com>
2017-04-01 13:49:49 -0400

    Did Drug data functions

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
-#from utils import map
-import drugs
-import labor
+from utils import data
 
-list_of_report = drugs.get_reports()
-print list_of_report 
+
 
 app = Flask(__name__)
 app.secret_key="idkjustsomethingrandom"
 
 @app.route('/')
 def root():
+	DrugList = makeDrugList()
+	PainList = makePainList()
+	AlcoholList = makeAlcoholList()
 	return render_template('index.html')
 
 if __name__ == '__main__':
