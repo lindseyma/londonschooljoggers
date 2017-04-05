6cc2cb4a1e1cf119fddc0a5ea99098072649d22e
jyang1737 <jyang1737@gmail.com>
2017-04-05 00:55:53 -0400

    basic layout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from utils import data, drugs, labor
 
 #import utils
@@ -6,13 +6,21 @@ from utils import data, drugs, labor
 app = Flask(__name__)
 app.secret_key="idkjustsomethingrandom"
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=["GET","POST"])
 def root():
-        d = data.compileList()
-        l = data.makeLaborList()
-        year = 2003
-	return render_template('index.html',d=d, year=year,l=l)
-
+        if 'year' not in session:
+                d = data.compileList()
+                l = data.makeLaborList()
+                session['year'] = 0
+                return render_template('index.html',d=d,l=l)
+        else:
+                d = data.compileList()
+                l = data.makeLaborList()
+                session['year'] = request.form['year']
+                year = session['year']
+                print year
+                return render_template('index.html',d=d, year=year,l=l)
+        
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
